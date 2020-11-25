959e5ae316cd5af8845edc2250934554a68c5091
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-25 19:03:38 +0000

    Insert left. Betty fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
-/*
-* binary_tree_insert_left: function to insert a left node
-* @parent = node parent
-* @value = value of the new node
+/**
+* binary_tree_insert_left - function to insert a left node
+* @parent: node parent
+* @value: value of the new node
 * Return: pointer to new node
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
