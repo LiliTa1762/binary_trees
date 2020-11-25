1a4ef6529d493dd1cd2e472a6b3ac6fbd73c8783
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-25 19:04:25 +0000

    Delete tree. Betty fixed

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,19 +1,18 @@
 #include "binary_trees.h"
 
-/*
-* binary_tree_delete: function to  that deletes an entire binary tree
-* @parent = root node of the tree
+/**
+* binary_tree_delete - function to that deletes an entire binary tree
+* @tree: pointer to the root node of the tree to delete
 * Return: 0
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+		return;
 
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
- 
-    free(tree);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
-    return;
+	free(tree);
+
 }
