312dbd0d127d0e9aa781b85a2a30bc455f7fe8a9
mauroxcf <maurox03@gmail.com>
2020-11-26 17:36:13 +0000

    function that finds the sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,34 +7,18 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	int node_1 = 0;
-	int node_2 = 0;
-	int left = 0;
-	int right = 0;
+	binary_tree_t *sibling = NULL;
 
-	if (node == NULL)
-		return (NULL);
+    if (node == NULL && node->parent == NULL)
+        return (NULL);
 
-	/* Compare the two nodes with the children of the node */
-	if (node->left && node->right)
-	{
-		left = node->left->n;
-		right = node->right->n;
+    else if (node->parent)
+        {
+            if (node->parent->left != node)
+                sibling = node->parent->left;
 
-		if (left == node_1 && right == node_2)
-			return (0);
-
-		else if (left == node_2 && right == node_1)
-			return (0);
-	}
-
-	/* Check the existence of left subtree */
-	if (node->left)
-		return (binary_tree_sibling(node->parent));
-
-	/* Check the existence of right subtree */
-	if (node->right)
-		return (binary_tree_sibling(node->parent));
-
-	return (node);
+            else
+                sibling = node->parent->right;
+        }
+    return (sibling);
 }
