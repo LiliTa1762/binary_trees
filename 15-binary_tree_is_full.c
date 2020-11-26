3a19f480425c978eb414696c6d2d40c0190a9254
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-26 19:55:24 +0000

    Tree is full. Fixed

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -17,7 +17,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	/* If both left and right are not NULL, and left & right subtrees are full */
 	if ((tree->left) && (tree->right))
-		return ((tree->left) && binary_tree_is_full(tree->right));
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
 	/* We reach here when none of the above if conditions work */
 	return (0);
